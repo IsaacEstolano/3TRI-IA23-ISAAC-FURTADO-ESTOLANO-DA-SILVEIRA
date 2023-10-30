7d451c028be917310e973a8fe80ec9700ba57641
IsaacEstolano <131707642+IsaacEstolano@users.noreply.github.com>
2023-10-30 17:56:11 +0000

    ygutyirtxtyixtycitycicgyigchcg

--- 001-ATIVIDADE-Menu Hamburger/main.js
+++ 001-ATIVIDADE-Menu Hamburger/main.js
@@ -5,3 +5,12 @@ btHamburger.addEventListener("click", () => {
   btHamburger.classList.toggle("opened")
   mainMenu.classList.toggle("opened")
 })
+//-------------------------------------------------
+const btDialog = document.querySelector(".bt-dialog")
+const btModal = document.querySelector(".bt-modal")
+const dialog = document.querySelector("dialog")
+
+console.log(btDialog,btModal,dialog)
+
+btDialog.addEventListener("click",() => dialog.show())
+btModal.addEventListener("click",() => dialog.showModal())
